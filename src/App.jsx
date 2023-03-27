c345e25c1b0f141cb8ce71eb4c911a405d1e5827
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-03-27 14:36:02 +0600

    Added styling and routing to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css'
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import NavBar from './components/NavBar';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <div className="App">
       <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Homepage</h1>} />
+          <Route exact path='/write' render={() => <h1>Write</h1>} />
+          <Route exact path='/saved' render={()=> <h1>Reading List</h1>} />
+          <Route exact path='/signin' render={()=> <h1>Sign in</h1>} />
+          <Route exact path='/signup' render={()=> <h2>Sign Up</h2>} />
+          <Route render={() => <p>Page not found</p>} />
+        </Switch>
+      </Container>
     </div>
   )
 }
 
 export default App
+
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,24 +4,56 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "../assets/logo.png"
 import styles from "../styles/NavBar.module.css"
+import { NavLink } from 'react-router-dom';
 
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} bg="light" expand="lg">
     <Container>
-      <Navbar.Brand href="#home" className="d-flex align-items-center">
-        <img src={logo} alt="logo" height="50" className="me-2" />
-        <span className="mt-1">Travelogue</span>
-      </Navbar.Brand>
+        <NavLink to="/">
+            <Navbar.Brand className="d-flex align-items-center">
+                <img src={logo} alt="logo" height="50" className="me-2" />
+                <span className="mt-1">Travelogue</span>
+            </Navbar.Brand>
+      </NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link href="#home">Articles</Nav.Link>
-          <Nav.Link href="#link">Reading List</Nav.Link>
-          <Nav.Link href="#link">Write</Nav.Link>
-          <Nav.Link>Sign in</Nav.Link>
-          <Nav.Link>Sign Up</Nav.Link>
+          <NavLink 
+            exact 
+            className={styles.NavLink} 
+            activeClassName={styles.Active} 
+            to='/'>
+                Articles
+          </NavLink>
+          <NavLink
+            className={styles.NavLink} 
+            activeClassName={styles.Active}
+            to="/saved"
+          >
+            Reading List
+          </NavLink>
+          <NavLink 
+            className={styles.NavLink} 
+            activeClassName={styles.Active}
+            to="/write"
+          >
+            Write
+          </NavLink>
+          <NavLink 
+            className={styles.NavLink} 
+            activeClassName={styles.Active} 
+            to="/signin">
+                Sign in
+          </NavLink>
+          <NavLink
+            className={styles.NavLink} 
+            activeClassName={styles.Active} 
+            to="/signup"
+          >
+                Sign Up
+          </NavLink>
         </Nav>
       </Navbar.Collapse>
     </Container>
